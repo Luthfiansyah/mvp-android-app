d6c41b82caff5fa7507e33d1ffddd34e1c3ca559
Moh Reza Luthfiansyah <rezaluthfiansyah@ruangguru.com>
2020-07-15 16:55:39 +0700

    implement fragments

---
+++ app/src/main/java/com/jalanesia/mytrip/PagerViewAdapter.kt
@@ -0,0 +1,36 @@
+package com.jalanesia.mytrip
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.jalanesia.mytrip.fragments.*
+
+internal class PagerViewAdapter(fm: FragmentManager?) : FragmentPagerAdapter(fm!!) {
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                HomeFragment()
+            }
+            1 -> {
+                SearchFragment()
+            }
+            2 -> {
+                AddFragment()
+            }
+            3 -> {
+                NotificationFragment()
+            }
+            4 -> {
+                ProfileFragment()
+            }
+            else ->  Fragment()
+        }
+    }
+
+    override fun getCount(): Int {
+
+        return 5
+    }
+
+}
+
